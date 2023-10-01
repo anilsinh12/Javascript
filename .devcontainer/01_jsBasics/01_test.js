4ccac64b827e4d7260b97fd0cf9ccf296dbf4030
Kushwah Anilsinh <131366487+anilsinh12@users.noreply.github.com>
2023-10-01 12:59:05 +0000

    array part

--- .devcontainer/01_jsBasics/01_test.js
+++ .devcontainer/01_jsBasics/01_test.js
@@ -105,21 +105,8 @@ Datatypes ---
             10/01/2023
 
 */
-                    //slice               vs                   splice
-            //    it cant change original array         Change original array                
-        const myArr2 = new Array(1, 2, 3, 4)
-        const myArr = [0, 1, 2, 3, 4, 5]
-        console.log("A ", myArr);
+                   
 
-        const myn1 = myArr.slice(1, 3)
-
-        console.log(myn1);
-        console.log("B ", myArr);
-
-
-        const myn2 = myArr.splice(1, 3)
-        console.log("C ", myArr);
-        console.log(myn2);
 /*
 
 */
---
+++ .devcontainer/01_jsBasics/02_array.js
@@ -0,0 +1,45 @@
+  //slice               vs                   splice
+            //    it cant change original array         Change original array    
+//             A  [ 0, 1, 2, 3, 4, 5 ]                      
+//                                                        C  [ 0, 4, 5 ]
+//             [ 1, 2 ]                                   [ 1, 2, 3 ]
+//             B  [ 0, 1, 2, 3, 4, 5 ]        
+
+
+/*
+
+      const myArr2 = new Array(1, 2, 3, 4)
+
+        const myArr = [0, 1, 2, 3, 4, 5]
+        console.log("A ", myArr);
+
+        const myn1 = myArr.slice(1, 3)
+
+        console.log(myn1);
+        console.log("B ", myArr);
+
+
+        const myn2 = myArr.splice(1, 3)
+        console.log("C ", myArr);
+        console.log(myn2);
+
+
+*/
+
+/*
+                                      Part -2
+                                      
+                                      
+*/
+/*
+
+*/
+/*
+
+*/
+
+
+/*
+
+*/
+        
