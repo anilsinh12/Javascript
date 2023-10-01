615345bc8742949c820505fa3c0740ea40df8433
Kushwah Anilsinh <131366487+anilsinh12@users.noreply.github.com>
2023-10-01 08:29:01 +0000

    basic detail of java script

---
+++ .devcontainer/01_jsBasics/01_test.js
@@ -0,0 +1,48 @@
+ 
+/*
+console.log("java")
+
+
+const s1 = "123"
+var s2 = "as"
+let s3 = "sing"
+s4 = "Anil"
+
+
+// s1 = "321"
+s2 =  "sa"
+s3 = "ASING"
+s4 = "anil"
+let s5; // undefined --means valiue is not defined
+console.table([s1,s2,s3,s4,s5])
+
+Always use let varible types
+
+null -- is stand alone value
+-------------------------------------
+*/
+
+/*
+ 
+*/
+
+
+/*
+
+*/
+
+
+
+/*
+
+*/
+
+
+
+/*
+
+*/
+
+/*
+
+*/
