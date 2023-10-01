30dc529482b33b18744138aef60ffa73e598d72f
Kushwah Anilsinh <131366487+anilsinh12@users.noreply.github.com>
2023-10-01 12:48:56 +0000

    here some Basic Array

--- .devcontainer/01_jsBasics/01_test.js
+++ .devcontainer/01_jsBasics/01_test.js
@@ -15,34 +15,111 @@ s3 = "ASING"
 s4 = "anil"
 let s5; // undefined --means valiue is not defined
 console.table([s1,s2,s3,s4,s5])
-
+console.table(typeof [s1,s2,s3,s4,s5]) // object
 Always use let varible types
 
 null -- is stand alone value
 -------------------------------------
+
+*/
+/*
+
+let s1 = "MacendLess"
+let s2 = " John" 
+let s3 = s1 + s2
+console.log(s3);
+-------------------------------------
+
+Datatypes ---
+        primitive ---String,number,Boolean,Undefined,null,BigInt
+        NoN-primitive(refrence type)----array ,object,Function
+
+        Special Data Types:
+
+            Symbol: Represents a unique and immutable value, primarily used as object property keys. Symbols help avoid property name collisions. Example: let uniqueSymbol = Symbol("description");
+           
+         Other Data Types:
+
+             JavaScript also provides a few other data types, such as BigInt for handling large integers and Map, Set, and WeakMap for advanced data structures.
+
+             function ->     datatype    function 
+            null    ->    datatype    Object
+            ================================================================================
+
+            primitive( Stack Memory- we can get only  copy of orignal value)    and     Non primitive( Heap memorry-- we can get references of original value )
+*/
+/*  
+            let s1 = "hello"
+            let s2  =" world"
+            let s3 =  "hello" + " world"
+            console.log(s3)
+
+
+            // New way 
+                using backtics ` `
+
+            console.log(`${s1} and ${s2}`)
+
+            //output :
+                    hello world
+                    hello and  world
+
+
+
+        --let s5 =  new String("anil")
+
+        let s5 =  new String("anil")
+        console.log(s5); 
 */
 
 /*
- 
+    let dt = new Date();
+    console.log(dt);
+    console.log(dt.toString());
+    console.log(dt.toDateString());
+    console.log(dt.toLocaleString());
+    console.log(typeof dt)
+
+        output :
+            Sun Oct 01 2023 12:14:34 GMT+0000 (Coordinated Universal Time)
+            Sun Oct 01 2023
+            10/1/2023, 12:14:34 PM
+            object
+
+    let myDate = new Date("01-04-2023")
+    console.log(myDate.toDateString());  //Wed Jan 04 2023
+
+    let newDate = Date.now();
+    console.log(newDate.toLocaleString());
 */
+
 
 
 /*
+        let myDate = new Date()
+        console.log(myDate);
+        console.log(myDate.getMonth()+1 + "/" + myDate.getDay()+ 1+ "/"+myDate.getFullYear())
+
+        output :
+            2023-10-01T12:28:24.681Z
+            10/01/2023
 
 */
+                    //slice               vs                   splice
+            //    it cant change original array         Change original array                
+        const myArr2 = new Array(1, 2, 3, 4)
+        const myArr = [0, 1, 2, 3, 4, 5]
+        console.log("A ", myArr);
+
+        const myn1 = myArr.slice(1, 3)
+
+        console.log(myn1);
+        console.log("B ", myArr);
 
 
-
-/*
-
-*/
-
-
-
-/*
-
-*/
-
+        const myn2 = myArr.splice(1, 3)
+        console.log("C ", myArr);
+        console.log(myn2);
 /*
 
 */
